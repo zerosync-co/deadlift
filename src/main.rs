c5a847fc35399ff0f4d0486c24d6ba2a0ab38d94
Alex Dunne <alex12dunne@gmail.com>
2024-05-29 14:19:14 -0600

    add diesel

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use diesel::{
+    r2d2::{ConnectionManager, Pool},
+    SqliteConnection,
+};
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+
+const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations");
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let manager = ConnectionManager::<SqliteConnection>::new("database.sqlite");
+    let pool = Pool::<ConnectionManager<SqliteConnection>>::new(manager).expect("db pool");
+    let mut conn = pool.get().expect("db connection");
+    conn.run_pending_migrations(MIGRATIONS)
+        .expect("run pending migrations");
+
     HttpServer::new(|| App::new().service(status_handler))
         .bind(("0.0.0.0", 8080))?
         .run()
