580c89c28ab195dcfbdb1edfedd23f9234f1a70c
Alex Dunne <alex12dunne@gmail.com>
2024-05-29 14:04:00 -0600

    add actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(status_handler))
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
+}
+
+#[get("/status")]
+async fn status_handler() -> impl Responder {
+    HttpResponse::NoContent()
 }
