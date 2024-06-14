1a3bcd7e1159b32a27e1f8bac0faabf701685a1d
Alex Dunne <alex12dunne@gmail.com>
2024-06-14 16:31:33 -0600

    wip

--- examples/camara-project/create_qod_session/src/lib.rs
+++ examples/camara-project/create_qod_session/src/lib.rs
@@ -1,11 +1,27 @@
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, Response};
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
+pub async fn run(base_url: String, body: JsValue) -> Result<JsValue, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("POST");
+    opts.body(Some(&body));
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+    let url = format!("{}", base_url);
+
+    let request = Request::new_with_str_and_init(&url, &opts)?;
+
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+    // `resp_value` is a `Response` object.
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    // Convert this other `Promise` into a rust `Future`.
+    let json = JsFuture::from(resp.json()?).await?;
+
+    // Send the JSON response back to JS.
+    Ok(json)
 }
